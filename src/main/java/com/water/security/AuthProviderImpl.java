3db750c4cd96bbbc0a9d1bccc9563672a2e6dfab
Shayfutdinov Aidar <exoooy@gmail.com>
2015-11-14 14:04:25 +0300

    Configs

---
+++ src/main/java/com/water/security/AuthProviderImpl.java
@@ -0,0 +1,22 @@
+package com.water.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class AuthProviderImpl implements AuthenticationProvider {
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
+
+}

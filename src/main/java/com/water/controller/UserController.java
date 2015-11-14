d460bd9a31f2a03e0fd0b39b5d46b69e3ee0abec
Alexander Pliskin <alexnetmore@gmail.com>
2015-11-14 16:20:46 +0300

    [alexander] user-page

--- src/main/java/com/water/controller/UserController.java
+++ src/main/java/com/water/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.water.controller;
 
+import com.water.model.Client;
+import com.water.model.Provider;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,8 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @RequestMapping("/home")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        Client user1;
+        Provider user2;
+        try {
+            user1 = (Client) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("role", "client");
+            model.addAttribute("principal", user1);
+        } catch (Exception e) {
+            user2 = (Provider) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("role", "provider");
+            model.addAttribute("principal", user2);
+        }
         return "home";
     }
-
 }
--- src/main/webapp/WEB-INF/templates/home.ftl
+++ src/main/webapp/WEB-INF/templates/home.ftl
@@ -1,8 +1,15 @@
-<html>
-<head>
+<#include "main-template.ftl"/>
+<#macro m_body>
+    <#if role == "client">
+    principal.name
+    here client
+    <#--client profile-->
 
-</head>
-<body>
-<h1> Hello, client! </h1>
-</body>
-</html>
+    <#else>
+    principal.name
+    here profile
+    <#--provider profile-->
+
+    </#if>
+</#macro>
+<@main title="Регистрация"/>

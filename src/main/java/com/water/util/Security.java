0328ba401f9e7c78c663100bc6b992b2e5cc4315
thevietto <thevietto@gmail.com>
2015-11-14 20:17:42 +0300

    auth fix

--- src/main/java/com/water/util/Security.java
+++ src/main/java/com/water/util/Security.java
@@ -1,12 +1,18 @@
 package com.water.util;
 
 import com.water.model.User;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class Security {
 
     public static User getCurrentUser() {
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+        return (User) authentication.getPrincipal();
     }
 
 }

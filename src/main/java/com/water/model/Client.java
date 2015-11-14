fcd47fd7d20a9c73fec11350bdae005949b784df
Alexander Pliskin <alexnetmore@gmail.com>
2015-11-14 14:01:45 +0300

    [alexander] model+view

--- src/main/java/com/water/model/Client.java
+++ src/main/java/com/water/model/Client.java
@@ -1,4 +1,62 @@
 package com.water.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String adress;
+
+    @Column(nullable = true)
+    @OneToOne
+    private Provider provider;
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
 }
--- src/main/java/com/water/model/Provider.java
+++ src/main/java/com/water/model/Provider.java
@@ -1,4 +1,60 @@
 package com.water.model;
 
+import javax.persistence.*;
+import java.util.List;
+
 public class Provider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String adress;
+
+    @OneToMany
+    private List<Client> clients;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
